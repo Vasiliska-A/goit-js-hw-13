458e5f00aa2a358a4a635a57d0b7e9c659833b4a
Vasylyna Andybur <vasylyna1206@yahoo.com>
2021-08-01 17:08:48 +0300

    done paggination

--- src/index.js
+++ src/index.js
@@ -1,31 +1,26 @@
+// import PicturesApiService from './js/fetchimages';
+
 import { refs } from './js/refs';
 import { fetchImages } from './js/fetchimages';
 import imageCardTmpl from './templates/imagecard.hbs';
 import { Notify } from 'notiflix';
 import './sass/main.scss';
 
-// refs.searchForm.addEventListener('submit', fetchImages);
 refs.searchButton.addEventListener('click', onSearchBtnClick);
+refs.loadMMoreBtn.addEventListener('click', onLoadMore);
 
 let query = '';
-let pageNumber = 1;
-
-async function getGalleryMarkUp() {
-  query = refs.input.value;
-  const pictures = await fetchImages(query, pageNumber);
-  //   console.log(pictures.hits);
-  const galleryMarkUp = pictures.hits.map(imageCardTmpl).join('');
-  refs.galleryWrapper.insertAdjacentHTML('afterbegin', galleryMarkUp);
-}
+let pageNumber = '';
 
 function onSearchBtnClick(event) {
   event.preventDefault();
-  console.log('12313');
   query = refs.input.value;
+  pageNumber = 1;
+  refs.loadMMoreBtn.classList.add('shown');
   refs.galleryWrapper.innerHTML = '';
 
   if (query !== '') {
-    return fetchImages(query)
+    return fetchImages(query, pageNumber)
       .then(pictures => {
         if (pictures.hits.length === 0) {
           console.log(pictures.hits);
@@ -35,10 +30,35 @@ function onSearchBtnClick(event) {
         } else {
           getGalleryMarkUp();
           const totalHits = pictures.totalHits;
-          console.log(totalHits);
+          //   console.log(totalHits);
           Notify.info(`Hooray! We found ${totalHits} images.`);
         }
       })
       .catch(Error);
   }
+}
+
+async function getGalleryMarkUp() {
+  query = refs.input.value;
+  const pictures = await fetchImages(query, pageNumber);
+  //   console.log(pictures.hits);
+  const galleryMarkUp = pictures.hits.map(imageCardTmpl).join('');
+  refs.galleryWrapper.insertAdjacentHTML('beforeend', galleryMarkUp);
+}
+
+function onLoadMore(event) {
+  // event.preventDefault();
+  refs.loadMMoreBtn.classList.remove('shown');
+  pageNumber += 1;
+
+  fetchImages(query)
+    .then(pictures => {
+      if (pictures.hits.length === 0) {
+        console.log(pictures.hits);
+        Notify.failure('Sorry, there are no images matching your search query. Please try again.');
+      } else {
+        getGalleryMarkUp();
+      }
+    })
+    .catch(Error);
 }
--- src/js/fetchimages.js
+++ src/js/fetchimages.js
@@ -9,3 +9,17 @@ export async function fetchImages(query, pageNumber) {
   const response = await axios.get(URL);
   return response.data;
 }
+
+// export default class PicturesApiService {
+//   constructor() {
+//     this.query = '';
+//   }
+
+//   async fetchImages() {
+//     const KEY = '22642465-57d0411bfabfa4c5aaf497117';
+//     const params = `?image_type=photo&orientation=horizontal&safesearch=true&q=${query}&page=${pageNumber}&per_page=40`;
+//     const URL = `https://pixabay.com/api/?${params}&key=${KEY}`;
+//     const response = await axios.get(URL);
+//     return response.data;
+//   }
+// }
